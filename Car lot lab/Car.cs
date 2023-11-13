78ce513ff2fddacf9c19fe891eee8381f23335df
Benjamin Holmes <manbej.sholem@gmail.com>
2023-11-13 09:17:46 -0500

    Add project files.

---
+++ Car lot lab/Car.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_lot_lab
+{
+    public class Car
+    {
+        public string Make {  get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+
+        public Car()
+        {
+            Make = string.Empty;
+            Model = string.Empty;
+            Year = 0;
+            Price = 0;
+        }
+
+        public Car(string make, string model, int year, decimal price)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Make: {Make} Model: {Model} Year :{Year} Price: ${Price}";
+        }
+
+        public static List<Car> GetCarsList(List<Car> carsList)
+        {
+
+            var cars = new List<Car>();
+            return cars;
+        }
+
+        public static void ListCars(List<Car> carsList)
+        {
+            for (int i = 0; i < carsList.Count; i++)
+            {
+                Car car = carsList[i];
+                Console.Write($" Index:{i} ");
+                Console.WriteLine(car.ToString());
+                
+            }
+            
+        }
+
+        public static List<Car> Remove(int x, List<Car> carList)
+        {
+
+            carList.RemoveAt(x);
+            return carList;
+        }
+    }
+
+    public class UsedCar : Car
+    {
+        public double Mileage { get; set; }
+
+        public UsedCar(string make, string model, int year, decimal price, double mileage)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+            Mileage = mileage;
+        }
+
+        public override string ToString()
+        {
+            return $"Make: {Make} Model: {Model} Year :{Year} Price: {Price} Mileage: {Mileage}";
+        }
+    }
+}
---
+++ Car lot lab/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Car_lot_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var carsList = new List<Car>();
+
+
+            carsList.Add(new Car("Ford","Fusion",2010,30_000));
+            carsList.Add(new Car("Ford", "Escape", 2020, 26_000));
+            carsList.Add(new Car("Honda", "Civic", 2018, 27_000));
+            carsList.Add(new UsedCar("Mazda", "3", 2016, 20_000,10_000));
+            carsList.Add(new UsedCar("Ford", "Mustang", 2018, 33_000, 20_000));
+            carsList.Add(new UsedCar("Honda", "Accord", 2022, 27_000, 18_000));
+
+            
+            Car.ListCars(carsList);
+            int index = GetUserIndex();
+            Console.WriteLine(carsList[index]);
+            Car.Remove(index, carsList);
+            Car.ListCars(carsList);
+        }
+
+        static int GetUserIndex()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the index of the car you would like to buy ");
+                string userIndex = Console.ReadLine();
+                if (int.TryParse(userIndex, out int index) && index <= 5 && index >= 0)
+                {
+                    return index;
+                }
+
+                Console.WriteLine("Invalid input, press and key to try again");
+                Console.ReadKey();
+            }
+        }
+    }
+}
